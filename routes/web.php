8cc5d0014f13e459acf2ff3bf1d8557a9b4fe06d
cyanobacteria <vodomelkor@gmail.com>
2017-11-04 02:08:20 +0800

    create posts controller and routes

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/posts', '\App\Http\Controller\PostController@index');
+Route::get('/posts/{post}', '\App\Http\Controller\PostController@show');
+Route::get('/posts/create', '\App\Http\Controller\PostController@create');
+Route::post('/posts', '\App\Http\Controller\PostController@store');
+Route::get('/posts/{post}/edit', '\App\Http\Controller\PostController@edit');
+Route::put('/posts/{post}', '\App\Http\Controller\PostController@update');
+Route::get('/posts/delete', '\App\Http\Controller\PostController@delete');
