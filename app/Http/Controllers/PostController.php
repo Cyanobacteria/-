d4c68c823fe21c1929d25e0268a0324847261e8e
cyanobacteria <vodomelkor@gmail.com>
2017-11-07 04:59:05 +0800

    傳值釐清

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -27,9 +27,10 @@ class PostController extends Controller
         
      }
    
-     public function store()
-     {
-         $post = new Post;
+     public function store(Post $post)
+     {   //dd($post);
+         //$post = new Post;
+         //使用綁定是這樣寫？ 
          $post->title = request()->title;
          $post->content = request()->content;
          if($post->save() == true)
@@ -40,17 +41,29 @@ class PostController extends Controller
          else
          {   //這邊應該要順便回傳error message
          return redirect('posts/create');
-         }                
+         } 
+                        
      }
-     public function edit(/*Post $post*/)
-     {
+     public function edit(/*Post $post*/  /*$post*/)
+     {   //dd($post);
+         //dd(request()->post);
          $post_id = request()->post;
          $post = Post::find($post_id);
          return view('post/edit', compact('post'));
      }
-     public function update(/*Post $post*/)
-     {
-       //dd($post); 
+     public function update(/*Post $post*/ /*Request $request*/)
+     {   //No query results for model 不知道是哪邊沒寫好，讓默認設置不能用 
+         //其他方法都可以用，猜是因為route 沒用resource一個一個建可能會有問題，再驗證
+         //dd($post);
+         //dd($request);
+         /*
+         如果說方法有至少四種
+         Post $post >>連資料都直接塞好了
+         Request $request
+         request()
+         action name($var) 對應 /example/{var} $var直接拿來用，也會傳進來
+         哪種比較好？應該說優劣何在？        
+         */ 
          $post_id = request()->id;
          $post = Post::find($post_id);
          $post->title = request()->title; 
@@ -67,14 +80,15 @@ class PostController extends Controller
              return redirect("posts/{$post_id}/edit");
          } 
      }
-     public function delete($id)
+     public function delete(/*$id*/)
      { 
        /*
        1.傳值方式有幾種？ 原生php way 跟laravel way 差在哪裡 又各有哪些？
        2.http method get post put patch delete
        3.慣例默認設置 有哪些？（可以寫最少code）
        */  
-       if(Post::find($id)->delete() == true)
+       $post_id = request()->post;
+       if(Post::find($post_id)->delete() == true)
        {
            return redirect('posts');
            //要寫個flash message
--- resources/views/post/edit.blade.php
+++ resources/views/post/edit.blade.php
@@ -3,7 +3,7 @@
 
  <h1 class="my-4">編輯文章</h1>
 <br><!-- action 這個是put 方法 返回按鈕  -->
-<form action="{{url('/posts/{post}')}}" method="post">
+<form action="{{url('posts/{post}')}}" method="POST">
     {!! csrf_field() !!}
     <input type="hidden" name="_method" value="put" >
     <input type="hidden" name="id" value="{{ $post->id }}">
