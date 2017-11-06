636716c172ad3cc1035d3e4b63b3eb22559b0022
cyanobacteria <vodomelkor@gmail.com>
2017-11-06 19:43:24 +0800

    post CRUD 與 views 主功能完成

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -34,7 +34,8 @@ class PostController extends Controller
          $post->content = request()->content;
          if($post->save() == true)
          {
-             return redirect('posts');
+             return redirect('posts/#post' . $post->id);
+             
          }
          else
          {   //這邊應該要順便回傳error message
@@ -43,26 +44,28 @@ class PostController extends Controller
      }
      public function edit(/*Post $post*/)
      {
-        $post_id = request()->post;
-        $post = Post::find($post_id);
-        return view('post/edit', compact('post'));
+         $post_id = request()->post;
+         $post = Post::find($post_id);
+         return view('post/edit', compact('post'));
      }
      public function update(/*Post $post*/)
      {
        //dd($post); 
-        $post_id = request()->id;
-        $post = Post::find($post_id);
-        $post->title = request()->title; 
-        $post->content = request()->content;
+         $post_id = request()->id;
+         $post = Post::find($post_id);
+         $post->title = request()->title; 
+         $post->content = request()->content;
         
-        if($post->save() == true)
-        {
-            return redirect('posts');
-        }
-        else
-        {   //view('file path'); redirect('route url');
-            return redirect("posts/{$post_id}/edit");
-        } 
+         if($post->save() == true)
+         {   //返回單筆顯示
+             return redirect('posts/'. $post->id);
+             //返回主頁並定位至目標post
+             //return redirect('posts/#post' . $post->id);
+         }
+         else
+         {   //view('file path'); redirect('route url');
+             return redirect("posts/{$post_id}/edit");
+         } 
      }
      public function delete($id)
      { 
@@ -72,7 +75,10 @@ class PostController extends Controller
        3.慣例默認設置 有哪些？（可以寫最少code）
        */  
        if(Post::find($id)->delete() == true)
-       {echo "成功"; }
+       {
+           return redirect('posts');
+           //要寫個flash message
+       }
        else
        {echo "失敗";} 
      }
